4c7997fe4aae6e18b780e96148d5bff8cb1f7f98
Manthan Dhrana <dhranamanthan201318@gmail.com>
2025-05-17 16:41:05 +0530

    upload path file change

--- backend/middlewares/csvValidator.js
+++ backend/middlewares/csvValidator.js
@@ -1,9 +1,14 @@
-const multer = require('multer');
-const path = require('path');
+const fs = require('fs');
+const uploadDir = '/tmp/uploads';
+
+// ensure upload folder exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/uploads'); // /tmp is writeable in Lambda
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
